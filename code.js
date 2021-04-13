f314c361f9f15692f13568553c4c195e8c3edfb0
NYJP <39363839+NYJP@users.noreply.github.com>
2021-04-13 21:05:27 +0800

    Add files via upload

--- code.js
+++ code.js
@@ -1,121 +1,95 @@
+var size = 150;
 
-var board = document.getElementById('boardInner');
-var turn = -1;
-var situ = []
-var inputlock = false;
-var root = document.documentElement;
-var newx = Math.round((Math.min(window.innerWidth,window.innerHeight) * 0.8) / 7);
+var file = txttoarray(readFile("vocab.csv"));
 
-$(window).resize(function() {
-  updatesq();
-});
-
-for (var x =0; x < 7; x++){
-  situ.push([0,0,0,0,0,0])
+for (var l=0; l<file.length; l++){
+    var word = document.createElement("DIV");
+    word.className = "word"
+    for (var i=0; i<file[l].length; i++){
+        var element = document.createElement("DIV");
+        element.innerHTML = file[l][i];    
+        if (i>0) element.className = "boxelements"                
+        word.appendChild(element);                               
+    }
+    document.getElementById("list").appendChild(word);
 }
 
-const renderBoard = () =>{
-  for (var i = 5; i >= 0; --i){
-    var row = document.createElement('DIV');
-    row.className = 'row';
-    for (var j = 0; j < 7; ++j){
-      var square = document.createElement('DIV');
-      square.className = 'square';
-      square.id = 'sq' + String(j) + String(i);
-      square.style.left = String(j*newx) + 'px';
-      square.style.top = String((5-i)*newx)+'px';
-
-      row.appendChild(square);
+function txttoarray(txt){
+    var temp = '';
+    var line = [];
+    var result = [];
+    var chr = '';
+    for (var i=0; i<txt.length; i++){
+        chr = txt.charAt(i);
+        if (chr == ','){
+            line.push(temp);
+            temp = '';
+        }else if (chr == '\n'){
+            line.push(temp);
+            temp = '';
+            result.push(line);
+            line = [];
+        }else{
+            temp += chr;
+        }
     }
-    board.appendChild(row);
-  }   
+    line.push(temp);
+    result.push(line);
+    return result;
+}
 
-  for (var i = 0; i < 7; ++i){
-    var button = document.createElement('button');
-    button.className = 'buttons';
-    button.id = 'col' + String(i);
-    board.appendChild(button);
+function readFile(file){
+    var f = new XMLHttpRequest();
+    var res = '';
+    f.open("GET", file, false);
+    f.onreadystatechange = function ()
+    {
+        if(f.readyState === 4)
+        {
+            if(f.status === 200 || f.status == 0)
+            {
+                res= f.responseText;
+            }
+        }
+    }
+    f.send(null);
+    return res;
+}
+
+function display(){
     
-  }
-  updatesq();
- 
 }
 
-renderBoard();
+function updateview(){
+    document.documentElement.style.setProperty('--size',String(size) +'px');
+    document.documentElement.style.setProperty('--fsize',String(size/10) +'px');
 
-document.querySelectorAll('button').forEach(function(element){
-  
-  element.addEventListener('click',function(){
-    if (!(inputlock)){
-      console.log(element.id);
-      turn ++;
-      inputlock = true;
-      place(parseInt(element.id.substr(3),10));
+    if (document.getElementById('mode').innerHTML == 'Mode: List'){
+        document.documentElement.style.setProperty('--boxorline',String(size/15*2) +'px');
+    }else{
+        document.documentElement.style.setProperty('--boxorline',String(size) +'px');
     }
-  })
-  
-})
+}
 
-function place(col){
-  var row = -1;
-  for (var index=0; index<6; index++){
-    if (situ[col][index] === 0){
-      situ[col][index] = turn%2+1;
-      row = index;
-      break;
+document.getElementById('mode').onclick = function(){
+    if (document.getElementById('mode').innerHTML == 'Mode: Box'){
+        document.documentElement.style.setProperty('--boxorlineele','none');
+        document.getElementById('mode').innerHTML = 'Mode: List';
+    }else{
+        document.documentElement.style.setProperty('--boxorlineele','block');
+        document.getElementById('mode').innerHTML = 'Mode: Box';
     }
-  }
-  if (row === -1){
-    turn --;
-    inputlock = false;
-  }else{
-    ani(col,5,row,turn);
-  }
-  
+    updateview()
 }
 
-
-
-function ani(col,row,trow,color){
-  console.log(turn);
-  if (trow === row){
-    if (color%2 === 0) document.getElementById('sq' + String(col) + String(row)).style.backgroundImage = "url('red.png')";
-    else document.getElementById('sq' + String(col) + String(row)).style.backgroundImage = "url('blue.png')";
-    inputlock = false;
-    return false;
-  }
-
-  console.log(document.getElementById('sq' + String(col) + String(row)).style.backgroundImage);
-  if (document.getElementById('sq' + String(col) + String(row)).style.backgroundImage === 'url("blue.png")' || document.getElementById('sq' + String(col) + String(row)).style.backgroundImage === 'url("red.png")'){
-    console.log('mt detect');
-    document.getElementById('sq' + String(col) + String(row)).style.backgroundImage = 'url("empty.png")';
-    ani(col,row-1,trow,color);
-    return false;
-  }
-  console.log('no detect');
-  if (color%2 === 0){
-    document.getElementById('sq' + String(col) + String(row)).style.backgroundImage = "url('red.png')";
-  }else{
-    document.getElementById('sq' + String(col) + String(row)).style.backgroundImage = "url('blue.png')";
-  }
-  setTimeout(function(){
-    ani(col,row,trow,color);
-  }, 100);
-  
+document.getElementById('zoomin').onclick = function(){
+    size+=30;
+    updateview()
 }
 
-function updatesq(){
-  newx = Math.round((Math.min(window.innerWidth,window.innerHeight) * 0.8) / 7);
-  console.log(newx);
-  root.style.setProperty('--x', String(newx) + 'px');
-  root.style.setProperty('--y', String(newx*6) + 'px');
-  root.style.setProperty('--y2', String(newx*7) + 'px');
-  for (var i = 5; i >= 0; --i){
-    for (var j = 0; j < 7; ++j){
-      square = document.getElementById('sq' + String(j) + String(i));
-      square.style.left = String(j*newx) + 'px';
-      square.style.top = String((5-i)*newx)+'px';
-    }
-  }   
-
+document.getElementById('zoomout').onclick = function(){
+    if (size > 100) size-=30;
+    updateview()
 }
+
+display();
